797ec3cf43713017f03a9b7791d09d4e1b98d7a1
Rustemt <rustemt@gmail.com>
2018-02-21 12:29:32 +0300

    Update Validator.swift

--- Validator.swift
+++ Validator.swift
@@ -3,7 +3,6 @@
 //
 //	Create by rustem on 29/11/2017
 //	Copyright © 2017. All rights reserved.
-//	Model file generated using JSONExport: https://github.com/Ahmed-Ali/JSONExport
 
 import Foundation 
 import ObjectMapper
